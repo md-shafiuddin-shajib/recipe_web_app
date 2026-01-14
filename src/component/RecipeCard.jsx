8a378896fae937d3bfc71e4fc6837167d329a232
md-shafiuddin-shajib <mdshafiuddinshajib@gmail.com>
2026-01-14 23:48:41 +0600

    Add Recipe Card and Slder.

--- src/component/RecipeCard.jsx
+++ src/component/RecipeCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const RecipeCard = () => {
+const RecipeCard = ({ meal }) => {
   return (
-    <div>RecipeCard</div>
-  )
-}
+    <div className="relative bg-gray-900 rounded-xl shadow-xl shadow-black/50 overflow-hidden transform transition-all group duration-500 border border-gray-800 hover:shadow-blue-600/30 ">
+      {/* Hover Glow */}
+      <div className="absolute inset-0 border-2 border-transparent group-hover:border-blue-600/80 transition-all duration-500"></div>
+      <div className="flex justify-center items-center p-5">
+        <img
+          src={meal.strMealThumb}
+          alt="meals image"
+          className="size-60 rounded-xl border border-yellow-400 transition-all transform group-hover:scale-105 duration-500"
+        />
+      </div>
+      <div className="p-2 text-center">
+        <h3 className="sm:whitespace-nowrap text-xl pb-3 font-bold text-gray-100 mb-1 group-hover:text-blue-400 transition-all duration-500 transform group-hover:scale-105 overflow-hidden ">
+          {meal.strMeal}
+        </h3>
+      </div>
+    </div>
+  );
+};
 
-export default RecipeCard
+export default RecipeCard;
--- src/component/RecipeSlider.jsx
+++ src/component/RecipeSlider.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import { useFetch } from './useFetch';
+import React from "react";
+import { useFetch } from "./useFetch";
+import RecipeCard from "./RecipeCard";
+import Slider from "react-slick";
+import { Clock, Loader} from "lucide-react";
 
-
-const RecipeSlider = ({title, fetchUrl}) => {
-  const {data, loading, error} = useFetch(fetchUrl);
-  console.log(data, error, loading);
-  return (
-    <div>RecipeSlider</div>
+const RecipeSlider = ({ title, fetchUrl }) => {
+  const { data, loading, error } = useFetch(fetchUrl);
+  console.log(data?.meals);
+  const meals = data?.meals || [];
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+  if(loading) return (
+    <div>< Loader className="animate-spin inline-block mr-2 text-blue-400"/></div>
   )
-}
+  if(error) return <div></div>
+  return (
+    <section className=" mt-2 max-w-7xl mx-auto">
+      <h2 className="text-3xl font-extrabold text-gray-100 mb-6 tracking-tight border-l-4 border-amber-300 pl-4 flex items-center">
+        <Clock className="size-6 mr-3 text-blue-500" />
+        {title}
+      </h2>
+      <div style={{width:'90%', margin:'auto', padding:'10px'}}>
+        <Slider {...settings}>
+        {meals.map((meal) => (
+          <div className="px-10 flex justify-center" key={meal.idMeal}>
+            <RecipeCard meal={meal} />
+          </div>
+        ))}
+        </Slider>
+      </div>
+    </section>
+  );
+};
 
-export default RecipeSlider
+export default RecipeSlider;
